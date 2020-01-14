521a4ca1da3e8102067d49df95ebf31463e4bd0e
jiyoun-kurly <jiyoun.seo@kurlycorp.com>
2020-01-14 15:45:55 +0900

    tabview 구현

--- 1-vanilla/js/controllers/MainController.js
+++ 1-vanilla/js/controllers/MainController.js
@@ -1,6 +1,8 @@
 import FormView from '../views/FormView.js'
 import ResultView from '../views/ResultView.js'
+import TabView from '../views/TabView.js'
 import SearchModel from '../models/SearchModel.js'
+
 
 const tag = ['MainController']
 
@@ -8,11 +10,22 @@ export default {
     init() {
         console.log(tag, 'init()')
         FormView.setup(document.querySelector('form'))
-        .on('@submit', e => this.onSubmit(e.detail.input))
-        .on('@reset', e => this.onResetForm())
+            .on('@submit', e => this.onSubmit(e.detail.input))
+            .on('@reset', e => this.onResetForm())
+
 
         ResultView.setup(document.querySelector('#search-result'))
-        
+        TabView.setup(document.querySelector('#tabs'))
+            .on('@change', e => this.onChangeTab(e.detail.tabName))
+
+        this.selectedTab = '추천 검색어'
+        this.renderView();
+    },
+
+    renderView() {
+        console.log (tag, 'renderView()')
+        TabView.setActiveTab(this.selectedTab)
+        ResultView.hide()
     },
 
     search(query) {
@@ -30,10 +43,16 @@ export default {
 
     onResetForm() {
         console.log(tag, 'onResetForm()')
+        ResultView.hide()
     },
 
     onSearchResult(data) {
         ResultView.render(data)
+    },
+
+    onChangeTab(tabName) {
+        debugger
+
     }
     
 }  
